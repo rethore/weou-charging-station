903c73d5ee0b41d5039500e994eee3497297dcea
johncoffee <github2017@ragelse.dk>
2018-05-24 16:29:06 +0200

    build

---
+++ pwa/src/status.js
@@ -0,0 +1,68 @@
+import { html, render } from '../node_modules/lit-html/src/lit-html.js';
+import { doRequest } from './main.js';
+const delay = 3 * 1000;
+async function poll(loop) {
+    let json;
+    try {
+        json = await doRequest('status', document.querySelector('.charging-station-id').value);
+    }
+    catch (e) {
+        loop = false;
+        console.error(e);
+        console.error("Stopped polling");
+    }
+    if (json) {
+        update(json);
+    }
+    if (loop) {
+        setTimeout(() => poll(loop), delay);
+    }
+}
+export function init() {
+    let q = location.search;
+    q = q.substr(q.indexOf("id=") + 3);
+    const input = document.querySelector('input');
+    input.value = q;
+    update({
+        co2: -1,
+        price: -1,
+        balance: -1,
+        charging: false,
+        connected: false,
+    });
+    poll(true);
+}
+function update(state) {
+    const template = html `
+  <div>
+    <p>CO2/kWh: ${state.co2}</p>
+    <p>Price: ${state.price} SVLN = 1 kWh</p>
+    <p>Balance on the station: <span>${state.balance}</span> SVLN
+    
+    <p>
+      <button class="button expanded large secondary">
+        ${!state.connected ? "CONNECT CAR" :
+        state.charging ? 'CHARGING' : ''}      
+      </button>
+  </div>
+  
+  <button class="button expanded large primary charging-controls__start ${state.charging ? 'hide' : ''}">START</button>
+  <button class="button expanded large secondary charging-controls__stop ${state.charging ? '' : 'hide'}">STOP</button>
+`;
+    render(template, document.querySelector('charging-status'));
+    const startBtn = document.querySelector('.charging-controls__start');
+    startBtn.disabled = (state.balance === 0);
+    document.querySelector('.charging-controls__start').addEventListener('click', async (evt) => {
+        startBtn.disabled = true;
+        await doRequest('start', document.querySelector('.charging-station-id').value);
+        await poll(false);
+        startBtn.disabled = false;
+    });
+    document.querySelector('.charging-controls__stop').addEventListener('click', async (evt) => {
+        const btn = evt.target;
+        btn.disabled = true;
+        await doRequest('stop', document.querySelector('.charging-station-id').value);
+        await poll(false);
+        btn.disabled = false;
+    });
+}
