0a1ac8c8b86804722f21c588fa76fc28fd583fcb
johncoffee <github2017@ragelse.dk>
2018-05-28 23:19:36 +0200

    remove await

--- charging-station/api.ts
+++ charging-station/api.ts
@@ -2,6 +2,7 @@ import * as Koa from 'koa'
 import { Context } from 'koa'
 import { ChargingStation } from './hardware.js'
 import { getBalanceOf, transferFrom } from './eth.js'
+import { httpRequest } from './http-request.js'
 const cors = require('koa-cors')
 const app = new Koa()
 
@@ -26,11 +27,16 @@ app.use( cors({
 
 app.use( router)
 
-app.listen((process.env.PORT || 3000), async () => {
+app.listen((process.env.PORT || 3000),  async() => {
   console.log("App listening on " + (process.env.PORT || 3000))
   if (process.argv[2] == 'test') {
-    const station = new ChargingStation('0x51f8a5d539582eb9bf2f71f66bcc0e6b37abb7ca')
-    console.log(await station.status())
+    try {
+      const result = await httpRequest(`http://localhost:3000/status?id=` + '0x51f8a5d539582eb9bf2f71f66bcc0e6b37abb7ca')
+      console.log(result.statusCode, result.json || result.body)
+    }
+    catch (e) {
+      console.error(e)
+    }
   }
 })
 
@@ -61,7 +67,7 @@ routes.set('/status', async (ctx:Context) => {
   const id = ctx.request.query.id
   console.assert(!!id, `missing query parameter 'id'`)
 
-  const station:ChargingStation = await new ChargingStation(id)
+  const station:ChargingStation = new ChargingStation(id)
   const response = await station.status()
 
   const balance = Math.round( Math.random() * 10000) //await getBalanceOf(ctx.request.query.id)
