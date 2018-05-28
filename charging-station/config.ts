fefafaf1282011bcc2366852a01a5ddee1819339
johncoffee <github2017@ragelse.dk>
2018-05-28 19:37:51 +0200

    enter correct keys for the chiemgauer contract and account

--- charging-station/config.ts
+++ charging-station/config.ts
@@ -1,4 +1,3 @@
-
-export const privateKey = `0x` + `51c2d5080641ca81c709862afc75065fc1e71cd6b3a8106c98a75045dc6cee47`
+export const privateKey = `0x` + `6724b72fdbf512ff825787602b761d2f9983c7e828ea905284f43ea9a19faa01` // 0xbd65990e92e07567c472e4a6a24291bf6aefcdb9
 export const networkId = 1337
-export const contractAddress = '0x6c041fb1e17aa0e95af5b078c45c7397fe3caa0b'
+export const contractAddress = '0x6C041FB1E17Aa0e95af5b078C45c7397fe3cAA0b'
--- charging-station/eth.ts
+++ charging-station/eth.ts
@@ -1,32 +1,25 @@
 import Web3C from 'web3'
 import { EncodedTransaction, TransactionObject, TransactionReceipt, Tx } from 'web3/types.js'
-import { contractAddress, privateKey } from './config.js'
+import { contractAddress, privateKey } from './config'
 
 const Web3 = require('web3')
 
 const API_KEY = 'xSa977dElK0pyDw8ipCV'
-// const rpcUrl = `https://rinkeby.infura.io/${API_KEY}`
-const rpcUrl = `http://localhost:7545`
+const rpcUrl = `https://rinkeby.infura.io/${API_KEY}`
+// const rpcUrl = `http://localhost:7545`
 
 const contractArtifact = require('./abi.json')
 const balanceOf = 'balanceOf'
-// const mintAbi = contractArtifact.abi.find((method:any) => method.name === balanceOf)
-// console.assert(!!mintAbi, `Should've found ${balanceOf} in `, contractArtifact.abi)
+const mintAbi = contractArtifact.abi.find((method:any) => method.name === balanceOf)
+console.assert(!!mintAbi, `Should've found ${balanceOf} in `, contractArtifact.abi)
 
 const web3:Web3C = new Web3(rpcUrl)
 
 let gasPrice = web3.utils.toWei("2", 'shannon')
 
 export function getAddress () {
-  const owner = web3.eth.accounts.privateKeyToAccount(privateKey) as any // it cant find signTransaction
+  const owner = web3.eth.accounts.privateKeyToAccount(privateKey)
   return owner.address
-}
-
-export async function main (target:string) {
-  console.log(`token contract address ${contractAddress}`)
-  console.log(`from ${getAddress()}`)
-  // getBalanceOf(target)
-  // transferFrom()
 }
 
 export async function getBalanceOf(target:string):Promise<number> {
@@ -49,9 +42,19 @@ export async function transferFrom(from:string, to:string, amount:number):Promis
   return res
 }
 
+async function main (target:string):Promise<void> {
+  console.log(`token contract address ${contractAddress}`)
+  console.log(`from ${getAddress()}`)
+  getBalanceOf(target)
+    .then(val => console.log(val))
+}
+
 if (!module.parent) {
   console.assert(process.argv[2])
-  main(process.argv[2])
-    .then((results) => console.log(results),
-      (err:Error) => console.error(err.toString()))
+  try {
+    main(process.argv[2])
+  }
+  catch (e) {
+    console.error(e)
+  }
 }
